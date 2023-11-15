8ac3fa5ce64300309bc268914de7c3a40d63e23c
shirotaro3 <y.shiro2203@gmail.com>
2023-11-15 19:07:54 +0900

    Modify: response headerの設定追加, 軽微な修正

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,12 @@ const nextConfig = {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
   },
+  headers: async () => {
+    return [
+      { source: '/:path*', headers: [{ key: 'Content-Language', value: 'ja-JA' }] },
+      { source: '/:path*', headers: [{ key: 'Cache-Control', value: 'max-age=31557600' }] },
+    ]
+  },
 }
 
 module.exports = nextConfig
