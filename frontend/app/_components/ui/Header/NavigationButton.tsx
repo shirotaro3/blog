eed8b7fa3a09392175c5b9c5c5d86a6182384ca3
shirotaro3 <y.shiro2203@gmail.com>
2023-11-19 03:34:55 +0900

    Modify: SVGアイコン追加, 適切なアイコンに差し替え

--- frontend/app/_components/ui/Header/NavigationButton.tsx
+++ frontend/app/_components/ui/Header/NavigationButton.tsx
@@ -2,6 +2,7 @@
 
 import clsx from 'clsx'
 import { useHamburgerMenu } from '@/components/ui/NavigationMenu'
+import Icon from '@/public/svg/search.svg'
 
 export function NavigationButton() {
   const [, setIsOpen] = useHamburgerMenu()
@@ -12,7 +13,7 @@ export function NavigationButton() {
   // TODO: アイコン作る
   return (
     <button onClick={handleClick} className={clsx('font-bold text-xl text-white drop-shadow')}>
-      三
+      <Icon className={clsx('w-5 h-5', 'lg:w-6 lg:h-6')} />
     </button>
   )
 }
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
+  images: { unoptimized: true, disableStaticImages: true },
   experimental: {
     scrollRestoration: true,
   },
@@ -10,6 +10,17 @@ const nextConfig = {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
     NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID: process.env.GOOGLE_MEASUREMENT_ID,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    })
+    return config
   },
 }
 
